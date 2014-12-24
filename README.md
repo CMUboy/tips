929c12e195ccc04f80f9badd2a1b3973bfb9f57b
Joseph Ku <jku@duettoresearch.com>
2014-12-23 22:26:48 -0800

    Completed CodePath iOS Bootcamp Pre-Work Tip Calculator Application.

---
+++ README.md
@@ -0,0 +1,24 @@
+# CodePath iOS Bootcamp Pre-Work Tip Calculator Application
+
+
+This is a simple Swift iOS application that calculates the tip and total amounts for a given bill amount.
+
+Time spent: 2.5 hours spent in total (including documentation)
+
+Completed user stories:
+
+ * [x] Required: User can enter a bill amount
+ * [x] Required: User can select from a preset list of tip percentages
+ * [x] Required: The application updates the tip and total amounts as the bill amount changes
+ * [x] Required: The application updates the tip and total amounts as the tip percentage changes
+ * [x] Required: The application dismisses the numeric keypad when the user clicks anywhere on the application other than the keypad itself
+ 
+Notes:
+
+Rather than using hardcoded tip percentages, the application obtains the tip percentages from the segmented control labels.
+
+Walkthrough of all user stories:
+
+![Video Walkthrough](tips_demo.gif)
+
+GIF created with [LiceCap](http://www.cockos.com/licecap/).
--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    var tipPercentages = [Double]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
+        // initialize the percentages array from the labels of the segmented control
+        
+        for var i = 0; i < tipControl.numberOfSegments; i++ {
+            var title: String! = tipControl.titleForSegmentAtIndex(i)
+            // drop the percentage sign, convert to decimal
+            tipPercentages.append(dropLast(title)._bridgeToObjectiveC().doubleValue / 100.0)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +38,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        println("User editing bill")
+        var billAmount = billField.text._bridgeToObjectiveC().doubleValue
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
 }
-
